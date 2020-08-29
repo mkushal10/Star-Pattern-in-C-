94ddbcd06d3687dff3f21eae44098ce157e9f9bb
Kushal Neupane <mkushal10@gmail.com>
2020-08-29 10:48:50 +0545

    A commit

---
+++ Print A/StarInA/StarInA/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarInA
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int row, column;
+
+            for (row = 0; row <= 7; row++)
+            {
+                for (column = 0; column <= 7; column++)
+                {
+                    if (((column == 1 || column == 5) && row != 0) || ((row == 0 || row == 3) && (column > 1 && column < 5)))
+                        Console.Write("*");
+                    else
+                        Console.Write(" ");
+                }
+                Console.Write("\n");
+            }
+            Console.Write("\n");
+            Console.Read();
+        }
+    }
+}
